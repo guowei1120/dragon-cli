64213f849d239a7cfe83146c3b745d36cc47a6b9
1476071989@qq.com <1476071989@qq.com>
2021-09-17 16:28:55 +0800

    所有文件删除

--- bin/index.js
+++
@@ -1,3 +0,0 @@
-#! /usr/local/bin/node node
-
-console.log(12312312);
--- src/utils/index.ts
+++
@@ -1,9 +0,0 @@
-export const add = (a, b) => {
-  console.log('ddddd')
-  return a + b
-}
-
-export const del = (a, b) => {
-  console.log('ccccc')
-  return a - b
-}
